7b0ad76a30248c15765339f323fc16ac31b46a8f
chavayaswanth2005 <yaswanthchava333@gmail.com>
2025-08-01 22:19:24 +0530

    ecommerce-frontend is created

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -52,8 +52,8 @@ const HomePage = () => {
             </div>
           ) : (
             <>
-              <Link to="/login"><button>Login</button></Link>
-              <Link to="/signup"><button>Sign Up</button></Link>
+              <Link to="/login"><button>yash</button></Link>
+              <Link to="/signup"><button>vishuu </button></Link>
             </>
           )}
         </div>
